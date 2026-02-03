5a157e3ae0557971a5905582b2b4036c4a17b00c
Bugged-Anshu <baibhabpradhan269@gmail.com>
2026-02-04 00:28:34 +0530

    script.js

--- script.js
+++ script.js
@@ -11,10 +11,31 @@ function goToQuestion() {
 function yesClicked() {
   document.getElementById("response").innerHTML =
     "You just made me the happiest person alive 😭❤️";
+
+  for (let i = 0; i < 30; i++) {
+    createHeart();
+  }
 }
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 2 + Math.random() * 3 + "s";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
 
 function moveButton(btn) {
   btn.style.position = "absolute";
   btn.style.top = Math.random() * 80 + "vh";
   btn.style.left = Math.random() * 80 + "vw";
 }
+
